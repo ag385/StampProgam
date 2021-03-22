bf25883965f8403fd882534420e904e4ad8e2e2d
Umbasa <umbasa@Umbasas-MacBook-Air.local>
2021-03-22 13:22:22 -0700

    Changed out to output

--- StampProgram.py
+++ StampProgram.py
@@ -9,7 +9,7 @@ Display the number of stamps                                        ----  output
 
 End
 """
-#Test input number 16 should out 3.2 and round up to 4
+#Test input number 16 should output 3.2 and round up to 4
 
 
 import math
@@ -18,3 +18,6 @@ number_of_stamps = number_of_sheets/5                                           
 round_stamps = math.ceil(number_of_stamps)                                              #Step 3
 print("The sheets of paper is " + str(number_of_sheets) + ".")                          
 print("The number of stamps needed is " + str(round_stamps) + ".")                      #Step 4
+
+
+
